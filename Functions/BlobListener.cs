74396304ba16e96a86e17dfcd0abca2cd6f830ea
VideoTestGroup <VideoTestGroup@gmail.com>
2022-11-27 15:45:36 +0200

    fix nameSpace

--- Functions/BlobListener.cs
+++ Functions/BlobListener.cs
@@ -24,6 +24,10 @@ public class BlobListener
         log.LogInformation($"[BlobListener] BlobProperties: {props}");
         BlobTags tags = new BlobTags(props, blobClient);
         ActivityAction activity = new ActivityAction(tags);
+
+        string blobNameWithoutExt = Path.GetFileNameWithoutExtension(blobName);
+        activity.Namespace = blobNameWithoutExt.Split("_").LastOrDefault() ?? "P3";
+
         Response response = await blobClient.WriteTagsAsync(tags);
         if(response.IsError)
             log.LogError(new EventId(1001), response.ToString());
