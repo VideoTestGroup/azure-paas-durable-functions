c903862aa488d3fd02b24442559c3f9e629c52ab
video test <videotestgroup@outlook.com>
2022-12-28 16:17:49 +0200

    change namespaces mapper

--- Functions/Extensions/EnvironmentVariablesExtensions.cs
+++ Functions/Extensions/EnvironmentVariablesExtensions.cs
@@ -2,10 +2,10 @@ namespace ImageIngest.Functions.Extensions;
 
 public static class EnvironmentVariablesExtensions
 {
-    const string NAMES_SPACES_MAPPER = "NamespacesMapper";
+    const string NAMES_SPACES_MAPPER = "Mapper";
 
     public static string GetNamespaceVariable(string @namespace)
     {
-        return Environment.GetEnvironmentVariable($"{NAMES_SPACES_MAPPER}:{@namespace}");
+        return Environment.GetEnvironmentVariable($"{@namespace}{NAMES_SPACES_MAPPER}");
     }
 }
