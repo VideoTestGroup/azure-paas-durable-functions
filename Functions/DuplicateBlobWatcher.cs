2c469e57d79abff0336bdefad906ec575938da67
VideoTestGroup <102216065+VideoTestGroup@users.noreply.github.com>
2023-03-05 12:27:54 +0200

    Update DuplicateBlobWatcher.cs

--- Functions/DuplicateBlobWatcher.cs
+++ Functions/DuplicateBlobWatcher.cs
@@ -1,29 +1,33 @@
-using System.Web;
-using Microsoft.AspNetCore.Mvc;
-
 namespace ImageIngest.Functions;
 
 public class DuplicateBlobWatcher
 {
     [FunctionName("DuplicateBlobWatcher")]
-    public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+    public static async Task<HttpResponseMessage> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest request,
         [DurableClient] IDurableEntityClient client,
         ILogger log)
         {
           string name = req.Query["name"];
           string key = req.Query["key"];
           string op = req.Query["op"] ?? "defalt";
+          log.LogInformation($"name: {name}, key: {key}, op: {op}");
           var entityId = new EntityId(name, key); 
           switch(op.ToLower())
           {
             case "reset":
-              client.SignalEntityAsync(entityId, "Reset");
-              return req.CreateResponse(HttpStatusCode.OK, stateResponse.EntityState);
-              break;
+                client.SignalEntityAsync(entityId, "Reset");
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(JsonConvert.SerializeObject(stateResponse.EntityState), Encoding.UTF8, "application/json")
+                };          
+                break;
             default:
-              EntityStateResponse<JObject> stateResponse = await client.ReadEntityStateAsync<JObject>(entityId);
-              return req.CreateResponse(HttpStatusCode.OK, stateResponse.EntityState);
+                EntityStateResponse<JObject> stateResponse = await client.ReadEntityStateAsync<JObject>(entityId);
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(JsonConvert.SerializeObject(stateResponse.EntityState), Encoding.UTF8, "application/json")
+                };          
           }
         }
 }
