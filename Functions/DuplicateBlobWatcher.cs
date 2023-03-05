5a422a21c7a19f1251f041844de77711f0a2c941
VideoTestGroup <102216065+VideoTestGroup@users.noreply.github.com>
2023-03-05 12:12:53 +0200

    Update DuplicateBlobWatcher.cs

--- Functions/DuplicateBlobWatcher.cs
+++ Functions/DuplicateBlobWatcher.cs
@@ -1,26 +1,26 @@
-using System.Web;
-using Microsoft.AspNetCore.Mvc;
-
 namespace ImageIngest.Functions;
 
-[FunctionName("DuplicateBlobWatcher")]
-public static async Task<IActionResult> Run(
-    [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
-    [DurableClient] IDurableEntityClient client,
-    ILogger log)
-    {
-      string name = req.Query["name"];
-      string key = req.Query["key"];
-      string op = req.Query["op"] ?? "defalt";
-      var entityId = new EntityId(name, key); 
-      switch(op.ToLower())
-      {
-        case "reset":
-          client.SignalEntityAsync(entityId, "Reset");
-          return req.CreateResponse(HttpStatusCode.OK, stateResponse.EntityState);
-          break;
-        default:
-          EntityStateResponse<JObject> stateResponse = await client.ReadEntityStateAsync<JObject>(entityId);
-          return req.CreateResponse(HttpStatusCode.OK, stateResponse.EntityState);
-      }
-    }
+public class DuplicateBlobWatcher
+{
+    [FunctionName("DuplicateBlobWatcher")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+        [DurableClient] IDurableEntityClient client,
+        ILogger log)
+        {
+          string name = req.Query["name"];
+          string key = req.Query["key"];
+          string op = req.Query["op"] ?? "defalt";
+          var entityId = new EntityId(name, key); 
+          switch(op.ToLower())
+          {
+            case "reset":
+              client.SignalEntityAsync(entityId, "Reset");
+              return req.CreateResponse(HttpStatusCode.OK, stateResponse.EntityState);
+              break;
+            default:
+              EntityStateResponse<JObject> stateResponse = await client.ReadEntityStateAsync<JObject>(entityId);
+              return req.CreateResponse(HttpStatusCode.OK, stateResponse.EntityState);
+          }
+        }
+}
