10507e6e0ccbbde2063c85d5b80593154e5d20f3
VideoTestGroup <102216065+VideoTestGroup@users.noreply.github.com>
2023-03-05 11:52:21 +0200

    Create DuplicateBlobWatcher.cs

---
+++ Functions/DuplicateBlobWatcher.cs
@@ -0,0 +1,21 @@
+[FunctionName("DuplicateBlobWatcher")]
+public static async Task<IActionResult> Run(
+    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+    [DurableClient] IDurableEntityClient client,
+    ILogger log)
+    {
+      string name = req.Query["name"];
+      string key = req.Query["key"];
+      string op = req.Query["op"] ?? "defalt";
+      var entityId = new EntityId(name, key); 
+      switch(op.ToLower())
+      {
+        case "reset":
+          client.SignalEntityAsync(entityId, "Reset");
+          return req.CreateResponse(HttpStatusCode.OK, stateResponse.EntityState);
+          break;
+        default:
+          EntityStateResponse<JObject> stateResponse = await client.ReadEntityStateAsync<JObject>(entityId);
+          return req.CreateResponse(HttpStatusCode.OK, stateResponse.EntityState);
+      }
+    }
