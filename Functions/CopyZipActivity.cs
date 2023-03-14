800568dd9a9929b054d8cea070ab9e41c51f4551
VideoTestGroup <102216065+VideoTestGroup@users.noreply.github.com>
2023-03-14 18:54:22 +0200

    Update CopyZipActivity.cs

--- Functions/CopyZipActivity.cs
+++ Functions/CopyZipActivity.cs
@@ -14,7 +14,7 @@ public class CopyZipActivity
         log.LogInformation($"[CopyZipActivity] Start copy {request.BlobName} to destination - {request.DistributionTarget.TargetName}, containerName - {request.ContainerName}");
         //var destClient = new BlobClient(request.DistributionTarget.ConnectionString, request.ContainerName, request.BlobName);
         //var destClient = new BlobClient(new Uri(request.BlobName), new AzureSasCredential(request.DistributionTarget.ConnectionString), new BlobClientOptions());
-        var response = new CopyZipResponse() { TargetName = request.DistributionTarget.TargetName };
+        var response = new CopyZipResponse() { TargetName = new Uri(request.DistributionTarget.TargetName) };
         
         BlobContainerClient targetContainerClient = new BlobContainerClient(request.DistributionTarget.ConnectionString);
             BlobClient destClient = targetContainerClient.GetBlobClient(request.BlobName);
@@ -27,7 +27,7 @@ public class CopyZipActivity
         try
         {
             //await destClient.UploadAsync(blobInput, overwrite: true);
-            BlobContentInfo info = await destClient.UploadAsync(blob, options);
+            BlobContentInfo info = await destClient.UploadAsync(blobInput, options);
         }
         catch (Exception ex)
         {
