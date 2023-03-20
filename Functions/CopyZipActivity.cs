73fa50b5c67b24b451b5f850dddc8f0ff4302e0e
VideoTestGroup <102216065+VideoTestGroup@users.noreply.github.com>
2023-03-20 13:00:16 +0200

    Update CopyZipActivity.cs

--- Functions/CopyZipActivity.cs
+++ Functions/CopyZipActivity.cs
@@ -12,22 +12,22 @@ public class CopyZipActivity
         ILogger log)
     {
         log.LogInformation($"[CopyZipActivity] Start copy {request.BlobName} to destination - {request.DistributionTarget.TargetName}, containerName - {request.ContainerName}");
-        //var destClient = new BlobClient(request.DistributionTarget.ConnectionString, request.ContainerName, request.BlobName);
+        var destClient = new BlobClient(request.DistributionTarget.ConnectionString, request.ContainerName, request.BlobName);
         //var destClient = new BlobClient(new Uri(request.BlobName), new AzureSasCredential(request.DistributionTarget.ConnectionString), new BlobClientOptions());
         var response = new CopyZipResponse() { TargetName = request.DistributionTarget.TargetName };
         
-        BlobContainerClient targetContainerClient = new BlobContainerClient(new Uri(request.DistributionTarget.ConnectionString));
-            BlobClient destClient = targetContainerClient.GetBlobClient(request.BlobName);
-            BlobUploadOptions options = new BlobUploadOptions
-            {
-                AccessTier = AccessTier.Hot,
-            };
+        //BlobContainerClient targetContainerClient = new BlobContainerClient(new Uri(request.DistributionTarget.ConnectionString));
+            //BlobClient destClient = targetContainerClient.GetBlobClient(request.BlobName);
+            //BlobUploadOptions options = new BlobUploadOptions
+            //{
+                //AccessTier = AccessTier.Hot,
+            //};
             
         
         try
         {
-            //await destClient.UploadAsync(blobInput, overwrite: true);
-            BlobContentInfo info = await destClient.UploadAsync(blobInput, options);
+            await destClient.UploadAsync(blobInput, overwrite: true);
+            //BlobContentInfo info = await destClient.UploadAsync(blobInput, options);
         }
         catch (Exception ex)
         {
