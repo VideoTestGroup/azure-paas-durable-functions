375dce44db874b2a751ecfa0d25c0282feac01c5
VideoTestGroup <102216065+VideoTestGroup@users.noreply.github.com>
2023-03-30 17:15:00 +0300

    Update BlobsCleaner.cs
    Added Batched Work in file

--- Functions/BlobsCleaner.cs
+++ Functions/BlobsCleaner.cs
@@ -7,6 +7,9 @@ public class BlobsCleaner
 
     public static TimeSpan DuplicateBlobsDeleteThreshold { get; set; } =
             TimeSpan.TryParse(Environment.GetEnvironmentVariable("DuplicateBlobsDeleteThreshold"), out TimeSpan span) ? span : TimeSpan.FromMinutes(5);
+    
+    public static TimeSpan BatchedBlobsRetryThreshold { get; set; } =
+            TimeSpan.TryParse(Environment.GetEnvironmentVariable("BatchedBlobsRetryThreshold"), out TimeSpan span) ? span : TimeSpan.FromMinutes(5);
 
     public static string DuplicateBlobsEntityName = Environment.GetEnvironmentVariable("DuplicateBlobsEntityName");
 
@@ -16,6 +19,7 @@ public class BlobsCleaner
         [TimerTrigger("0 */%BlobsDeleteTimer% * * * *")] TimerInfo myTimer,
         [Blob(Consts.FTPContainerName, Connection = "AzureWebJobsFTPStorage")] BlobContainerClient blobContainerClient,
         [DurableClient] IDurableClient durableClient,
+        [DurableClient] IDurableOrchestrationClient starter,
         ILogger log)
     {
         log.LogInformation($"[BlobsCleaner] Start delete zipped blobs at: {DateTime.Now}");
@@ -29,6 +33,18 @@ public class BlobsCleaner
         deleteQuery = BlobClientExtensions.BuildTagsQuery(isDuplicate: true, modifiedTime: DateTime.UtcNow.Subtract(DuplicateBlobsDeleteThreshold).ToFileTimeUtc());
         deleteCount = await blobContainerClient.DeleteByTagsAsync(deleteQuery);
         log.LogInformation($"[BlobsCleaner] {deleteCount} duplicate blobs cleaned successfully");
+        
+        log.LogInformation($"[BlobsCleaner] Start Retry Batched files blobs at: {DateTime.Now}");
+        string batchedQuery = BlobClientExtensions.BuildTagsQuery(status: BlobStatus.Batched, modifiedTime: DateTime.UtcNow.Subtract(BatchedBlobsRetryThreshold).ToFileTimeUtc());
+        var items = await client.FindBlobsByTagsAsync(batchedQuery);
+       
+        var result = items.GroupBy(x => new x.BatchId) .Select(group => new { BatchId = group.Key, BatchImg = group.Take(1) }) .ToList();
+        foreach (var group in result)
+        {
+            var activity = new ActivityAction() { Namespace = group.BatchImg.Namespace , BatchId = group.BatchImg.BatchId };
+            await starter.StartNewAsync(nameof(ZipperOrchestrator), activity);
+        }
+        
     }
 }
 
