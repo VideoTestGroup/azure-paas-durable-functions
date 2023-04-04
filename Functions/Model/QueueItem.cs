9f7bbc9a8564bbebc064cc7739d91f6eb39384e4
video test <videotestgroup@gmail.com>
2023-04-04 14:42:40 +0300

    adding service bus queue to flow

---
+++ Functions/Model/QueueItem.cs
@@ -0,0 +1,8 @@
+﻿namespace ImageIngest.Functions.Model;
+
+public class QueueItem
+{
+    public int deliveryCount { get; set; }
+    public DateTime enqueuedTimeUtc { get; set; }
+    public string messageId { get; set; }
+}
