62ec87f13bbda1d028809bd1b937dce8e0de3557
video test <videotestgroup@gmail.com>
2023-04-04 16:24:42 +0300

    logging

--- Functions/Model/EventGridItem.cs
+++ Functions/Model/EventGridItem.cs
@@ -3,8 +3,12 @@
 [DataContract]
 public class EventGridItem
 {
+    [DataMember(Name = "id")]
     public string Id { get; set; }
+    [DataMember(Name = "topic")]
     public string Topic { get; set; }
+    [DataMember(Name = "subject")]
     public string Subject { get; set; }
+    [DataMember(Name = "eventType")]
     public string EventType { get; set; }
 }
