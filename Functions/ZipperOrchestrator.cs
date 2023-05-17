3b01e3fc7045d756171ca5d53dc8dfd1998d4869
VideoTestGroup <102216065+VideoTestGroup@users.noreply.github.com>
2023-05-17 21:42:30 +0300

    Update ZipperOrchestrator.cs change to queue trigger

--- Functions/ZipperOrchestrator.cs
+++ Functions/ZipperOrchestrator.cs
@@ -3,11 +3,15 @@ public class ZipperOrchestrator
 {
     [FunctionName(nameof(ZipperOrchestrator))]
     public static async Task Run(
-        [OrchestrationTrigger] IDurableOrchestrationContext context,
+        [ServiceBusTrigger("batches", Connection = "ServiceBusConnection", AutoCompleteMessages=true)]
+            string myQueueItem,
+            Int32 deliveryCount,
+            DateTime enqueuedTimeUtc,
+            string messageId,
         ILogger log)
     {
         log.LogInformation($"[ZipperOrchestrator] OrchestrationTrigger triggered Function for InstanceId {context.InstanceId}");
-        var activity = context.GetInput<ActivityAction>();
+        var activity = new ActivityAction() { BatchId = myQueueItem };
 
         log.LogInformation($"[ZipperOrchestrator] Zipping files for activity: {activity}");
         bool? isSuccessfull = await context.CallActivityAsync<bool?>(nameof(Zipper), activity);
