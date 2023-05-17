836be2ee689dea5bbeeb8617ec696225486e3be0
VideoTestGroup <102216065+VideoTestGroup@users.noreply.github.com>
2023-05-17 17:29:52 +0300

    Update Consts.cs - files2

--- Functions/Consts/Consts.cs
+++ Functions/Consts/Consts.cs
@@ -6,7 +6,7 @@ public class Consts
     // When changing ContainerName make sure to change EventGrid's topic filter
     // Subject Begins With: blobServices/default/containers/files
 
-    public const string FTPContainerName = "files1";
+    public const string FTPContainerName = "files2";
     public const string ZipContainerName = "zip";
     public const string FailedZipsContainerName = "failed-zips";
 }
