d2668dc7a5ede1c239a7e0fba0d7638437f50c5d
VideoTestGroup <102216065+VideoTestGroup@users.noreply.github.com>
2023-05-21 15:02:37 +0300

    Update Consts.cs MS fix container name

--- Functions/Consts/Consts.cs
+++ Functions/Consts/Consts.cs
@@ -1,13 +1,10 @@
-namespace ImageIngest.Functions.Model;
-
 public class Consts
 {
     // IMPORTANT: 
     // When changing ContainerName make sure to change EventGrid's topic filter
     // Subject Begins With: blobServices/default/containers/files
 
-    public const string FTPContainerName = "files2";
-    public const string ZipContainerName = "zip";
-    public const string FailedZipsContainerName = "failed-zips";
+    public const string FTPContainerName = "files3-ms";
+    public const string ZipContainerName = "zip3-ms";
+    public const string FailedZipsContainerName = "failed-zips3-ms";
 }
-
