81a268f62c8768c3cd7bf01b31606320fa43fcee
VideoTestGroup <102216065+VideoTestGroup@users.noreply.github.com>
2023-05-21 17:30:02 +0300

    Create testtrigger.cs

---
+++ Functions/testtrigger.cs
@@ -0,0 +1,13 @@
+using Microsoft.Azure.WebJobs; 
+using Microsoft.Extensions.Logging; 
+using System.Threading.Tasks; 
+namespace ImageIngest.Functions { 
+public static class BatchProcessorFunction { 
+[FunctionName("BatchProcessorFunction")] 
+public static async Task RunAsync( 
+[ServiceBusTrigger("batches", Connection = "ServiceBusConnection", AutoCompleteMessages=true)] string message,
+ILogger logger) { 
+logger.LogInformation($"Received batch message: {message}");
+// Process the batch message asynchronously here 
+await Task.Delay(1000); // Example processing delay // You can also use await to call other async methods for processing logger.LogInformation("Batch message processed."); 
+} } }
