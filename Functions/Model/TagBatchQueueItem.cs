3970c3ae98ec8b1c7cff6dddb0d54f848fe06d5c
VideoTestGroup <102216065+VideoTestGroup@users.noreply.github.com>
2023-05-21 14:22:45 +0300

    Create TagBatchQueueItem.cs MS

---
+++ Functions/Model/TagBatchQueueItem.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageIngest.Functions.Model
+{
+    // this is the class that BlobListener insert into the queue, to be consumed by the zipper function
+    public class TagBatchQueueItem
+    {
+        public string Namespace { get; set; } = "default";
+        public string BatchId { get; set; }
+
+        public string Container { get; set; }
+
+        public List<string> FileNames {get;set; }
+
+    }
+   
+}
